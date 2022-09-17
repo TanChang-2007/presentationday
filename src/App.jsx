7ae1ef9b49ebf29769c71af04a1cc8fc2e0f1aa2
Changgg <tanaikchang@gmail.com>
2022-09-17 09:09:21 +0800

    Add somthing

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ function Home() {
   return (
     <div className="flex bg-gradient-to-r from-[#F9E7FE] to-[#DAFCFC]">
       <div className='absolute text-sm sm:text-md md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl m-7 select-none'>{moment().format('DD MMMM YYYY')}</div>
-      <Link to="/timer" className=' border-b-2 border-black border-dotted flex flex-row align-middle text-5xl absolute m-10 pt-40'><Icon icon="fluent:timer-28-regular" width="50" height="50" /><span className='pl-3'>Timer</span></Link>
+      <div className='flex flex-col'>
+        <a href='https://www.timeanddate.com/timer/'><span className=' border-b-2 border-black border-dotted flex flex-row align-middle text-5xl absolute m-10 pt-10'>Timer</span></a>
+        <a href='https://stopwatch.online-timers.com/online-stopwatch'><span className=' border-b-2 border-black border-dotted flex flex-row align-middle text-5xl absolute m-10 pt-28'>Stopwatch</span></a>
+        <a href='https://www.timeanddate.com/worldclock/'><span className=' border-b-2 border-black border-dotted flex flex-row align-middle text-5xl absolute m-10 pt-48'>World Clock</span></a>
+      </div>
       <div className='flex flex-1 sm:text-3xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl items-center justify-center h-screen select-none font-[DM-Sans]'>
-        {moment().format("LTS")}
+        {moment().format("LT")}
       </div>
     </div>
   )
 }
 
-function Timer() {
+function Ecommerce(){
   return(
-    <div className='flex bg-gradient-to-r from-[#F9E7FE] to-[#DAFCFC] h-screen w-full'>
-      <div className=''>
-        
-      </div>
-    </div>
+    <div></div>
   )
 }
 
@@ -33,7 +33,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="timer" element={<Timer />} />
+          <Route path="ecommerce" element={<Eccormerce />} />
         </Routes>
       </BrowserRouter>
     </div>
